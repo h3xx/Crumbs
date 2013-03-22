bc2ed097da7cddd2027b2082de5080ec6ae596c7
Dan Church <amphetamachine@gmail.com>
2013-03-22 15:59:17 -0500

    Implement determing and saving location information.

--- cgi-bin/Crumbs/Controller/Crumb.pm
+++ cgi-bin/Crumbs/Controller/Crumb.pm
@@ -17,34 +17,74 @@ sub new {
 sub get {
 	my ($self, $lat, $lon, $from_user, $stickpole, $limit) = @_;
 
-	require Crumbs::Tools;
-
-	# record last position
-	# make sure coords are numeric first
-	if (&Crumbs::Tools::is_numeric($lat, $lon)) {
-		$self->{'session'}->param('lastpos', [$lat,$lon]);
-	}
-}
-
-sub put {
-	my ($self, $lat, $lon, $stickpole) = @_;
-
-	require Crumbs::Tools;
-
-	# record last position
-	if (&Crumbs::Tools::is_numeric($lat, $lon)) {
-		$self->{'session'}->param('lastpos', [$lat,$lon]);
-	} else {
+	# get position
+	my $pos = $self->_pos($lat, $lon);
+	unless (defined $pos) {
 		return {
 			'result'=> 0,
-			'msg'	=> 'FIXME : Figure out where you are.',
+			'msg'	=> 'Failed to determine your position.',
 		};
 	}
 
 	return {
 		'result'=> 1,
+		'pos'	=> $pos,
 		'msg'	=> 'poop',
 	};
+}
+
+sub put {
+	my ($self, $lat, $lon, $stickpole) = @_;
+
+	# get position
+	my $pos = $self->_pos($lat, $lon);
+	unless (defined $pos) {
+		return {
+			'result'=> 0,
+			'msg'	=> 'Failed to determine your position.',
+		};
+	}
+
+	return {
+		'result'=> 1,
+		'pos'	=> $pos,
+		'msg'	=> 'poop',
+	};
+}
+
+# use trickery to determine position
+sub _pos {
+	my ($self, $lat, $lon) = @_;
+
+	require Crumbs::Tools;
+
+	# step 1: determine from given coordinates
+	my $pos;
+	if (&Crumbs::Tools::is_numeric($lat, $lon)) {
+		$pos = [$lat, $lon];
+		$self->{'session'}->param('lastpos', $pos);
+		return $pos;
+	}
+
+	# step 2: determine from last save coordinates
+	$pos = $self->{'session'}->param('lastpos');
+	return $pos if defined $pos;
+
+	# step 3: determine from IP address
+	my $cli_addr = $self->{'cgi'}->remote_addr;
+
+	unless (defined $cli_addr and $cli_addr !~ /^(127\.0\.0\.1|192\.168\.|10\.0)/) {
+		# local network address, can't use it
+		return undef;
+	}
+
+	use Geo::IP;
+	my $gi = Geo::IP->open('/usr/share/GeoIP/GeoIPCity.dat', GEOIP_STANDARD);
+	my $record = $gi->record_by_addr($cli_addr);
+	$pos = [ $record->latitude, $record->longitude ] if defined $record;
+
+	$self->{'session'}->param('lastpos', $pos);
+	return $pos;
 }
 
 =head1 AUTHOR
